9341e25a8826adfe769592ebd76eab02fb5653ab
Abhaya <shankarabhaya9@gmail.com>
2023-02-14 20:53:02 +0530

    setup done will start now

--- Cocktails/cocktails/src/App.jsx
+++ Cocktails/cocktails/src/App.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import "./App.css";
+
+// importing pages for the App
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Error from "./pages/Error";
 import SingleCocktail from "./pages/SingleCocktail";
 
+// importing Navbar
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
       <h3>Abhaya</h3>
+      {/* <Navbar /> */}
     </div>
   );
 }
