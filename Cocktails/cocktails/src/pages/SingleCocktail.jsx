bdbf42f19e887d7261e25078beac3dc2491e0189
Abhaya <shankarabhaya9@gmail.com>
2023-02-17 02:03:25 +0530

    finished

--- Cocktails/cocktails/src/pages/SingleCocktail.jsx
+++ Cocktails/cocktails/src/pages/SingleCocktail.jsx
@@ -1,59 +1,102 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Loading from "../components/Loading";
 import { useParams, Link } from "react-router-dom";
-const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
-const SingleCocktail = () => {
-  const [loading, setLoading] = useState(false);
-  const [cocktail, setCocktail] = useState(null);
+export default function SingleCocktail() {
+  const { id } = useParams();
+  const [loading, setLoading] = React.useState(false);
+  const [cocktail, setCocktail] = React.useState(null);
 
-  async function getCocktail() {
-    try {
-      const res = await fetch(`${url}${id}`);
-      const data = await res.json();
-      console.log(data);
-      if (data.drinks) {
-        const singleDrink = data.drinks;
-        {
-          singleDrink.map((drink) => {
-            const {
-              id: idDrink,
-              category: strAlcoholic,
-              img: strDrinkThumb,
-              ingrd1: strIngredient1,
-              ingrd2: strIngredient2,
-              ingrd3: strIngredient3,
-              ingrd4: strIngredient4,
-            } = drink;
-
-            return (
-              <>
-                <h1>{category}</h1>
-                <p>{ingrd1}</p>
-              </>
-            );
-          });
+  React.useEffect(() => {
+    setLoading(true);
+    async function getCocktail() {
+      try {
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+        );
+        const data = await response.json();
+        if (data.drinks) {
+          const {
+            strDrink: name,
+            strDrinkThumb: image,
+            strAlcoholic: info,
+            strCategory: category,
+            strGlass: glass,
+            strInstructions: instructions,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          } = data.drinks[0];
+          const ingredients = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          ];
+          const newCocktail = {
+            name,
+            image,
+            info,
+            category,
+            glass,
+            instructions,
+            ingredients,
+          };
+          setCocktail(newCocktail);
+        } else {
+          setCocktail(null);
         }
-      } else {
-        setLoading(false);
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
       setLoading(false);
     }
-  }
-
-  useEffect(() => {
     getCocktail();
-  }, []);
-
-  const { id } = useParams();
-  console.log(id);
-  return (
-    <div>
-      <h2>single cocktail page </h2>
-    </div>
-  );
-};
-
-export default SingleCocktail;
+  }, [id]);
+  if (loading) {
+    return <Loading />;
+  }
+  if (!cocktail) {
+    return <h2 className="section-title">no cocktail to display</h2>;
+  } else {
+    const { name, image, category, info, glass, instructions, ingredients } =
+      cocktail;
+    return (
+      <section className="section cocktail-section">
+        <Link to="/" className="btn btn-primary">
+          back home
+        </Link>
+        <h2 className="section-title">{name}</h2>
+        <div className="drink">
+          <img src={image} alt={name}></img>
+          <div className="drink-info">
+            <p>
+              <span className="drink-data">name :</span> {name}
+            </p>
+            <p>
+              <span className="drink-data">category :</span> {category}
+            </p>
+            <p>
+              <span className="drink-data">info :</span> {info}
+            </p>
+            <p>
+              <span className="drink-data">glass :</span> {glass}
+            </p>
+            <p>
+              <span className="drink-data">instructons :</span> {instructions}
+            </p>
+            <p>
+              <span className="drink-data">ingredients :</span>
+              {ingredients.map((item, index) => {
+                return item ? <span key={index}> {item}</span> : null;
+              })}
+            </p>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
