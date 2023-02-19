ac3cfb2cbd05928e9507908af0c3333149ac5978
Abhaya <shankarabhaya9@gmail.com>
2023-02-20 02:43:58 +0530

    login credentials

--- Spotify 2.0/spotify/src/App.js
+++ Spotify 2.0/spotify/src/App.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./components/Login/Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="app">
-      {/* <h1>Building Spotify clone from clever programming.</h1> */}
-      <Login />
-    </div>
-  );
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log(user);
+      });
+    }
+
+    console.log("I got token", _token);
+  }, []);
+
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
