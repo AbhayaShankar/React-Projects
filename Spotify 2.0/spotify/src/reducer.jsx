f8255546459b48d6a49cfdc20277bbc634c3bcc7
Abhaya <shankarabhaya9@gmail.com>
2023-02-21 03:38:36 +0530

    update

---
+++ Spotify 2.0/spotify/src/reducer.jsx
@@ -0,0 +1,22 @@
+const initialState = {
+  user: null,
+  playlist: [],
+  playing: false,
+  item: null,
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+    default:
+      return state;
+  }
+};
+
+export { initialState, reducer };
