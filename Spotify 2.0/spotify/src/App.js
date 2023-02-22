334cfca2ca3dde1232e94b92d2a186cf693eb74a
Abhaya <shankarabhaya9@gmail.com>
2023-02-22 13:02:37 +0530

    body dtratrted

--- Spotify 2.0/spotify/src/App.js
+++ Spotify 2.0/spotify/src/App.js
@@ -3,33 +3,43 @@ import "./App.css";
 import Login from "./components/Login/Login";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
-import Player from "./Player";
+import Player from "./components/Player/Player";
 import { useDataLayerValue } from "./DataLayer";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
-  const [{}, dispatch] = useDataLayerValue();
-
+  const [{ user, token }, dispatch] = useDataLayerValue();
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
       spotify.setAccessToken(_token);
 
       spotify.getMe().then((user) => {
-        console.log(user);
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
       });
     }
 
     console.log("I got token", _token);
   }, []);
+  console.log(user);
+  console.log(token);
 
-  return <div className="app">{token ? <Player /> : <Login />}</div>;
+  return (
+    <div className="app">
+      {token ? <Player spotify={spotify} /> : <Login />}
+    </div>
+  );
 }
 
 export default App;
--- Spotify 2.0/spotify/src/reducer.jsx
+++ Spotify 2.0/spotify/src/reducer.jsx
@@ -3,6 +3,8 @@ const initialState = {
   playlist: [],
   playing: false,
   item: null,
+  token:
+    "BQA6UFCt5sj3EFzf8Ax2XuoNpZiLiBVWtmfj4lzln8whwpw0sntL8nucJOye2zGMt8ePivzvZXDpDh1pF1at7T90iy8QcE2cChxb6F7i-Ju1R6kfND0Wr9-BtSYGpV0HF2SwJN0-wYgMfnrVPxUGkw-d91hhcsPzTAA6gef_F5D8TQtuUkfp2qM-1bbZRxm3FsOROjElEUTVJm7Ah3lPeQ",
 };
 
 const reducer = (state, action) => {
@@ -13,6 +15,11 @@ const reducer = (state, action) => {
       return {
         ...state,
         user: action.user,
+      };
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
       };
     default:
       return state;
