f38e0e3e9e98b3ea2cf8625d68c76a7d76323778
Abhaya <shankarabhaya9@gmail.com>
2023-03-06 19:45:55 +0530

    search filter completed designs left here and there

--- IMDB-clone/imdb-clone/src/components/header/Header.jsx
+++ IMDB-clone/imdb-clone/src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import imdb from "../../media/imdb.png";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 function Header({ search, handleChange, handleSearch }) {
   return (
@@ -16,7 +17,10 @@ function Header({ search, handleChange, handleSearch }) {
         <Link to="/movies/upcoming">Upcoming</Link>
       </div>
       <div className="header-right">
-        <SearchIcon className="header__search__icon" sx={{ color: "black" }} />
+        <SearchIcon
+          className="header__search__icon"
+          sx={{ color: "#c9c9c9" }}
+        />
         {/* <h3>Search Box</h3> */}
         <form className="form" type="search">
           <input
@@ -27,9 +31,11 @@ function Header({ search, handleChange, handleSearch }) {
             onKeyDown={handleSearch}
             placeholder="Enter your search"
           />
-
-          <button className="search__form__button">Search</button>
         </form>
+      </div>
+      <div className="header-right-right">
+        <h3>Abhaya</h3>
+        <AccountCircleIcon sx={{ fontSize: 32, color: "#c9c9c9" }} />
       </div>
     </div>
   );
