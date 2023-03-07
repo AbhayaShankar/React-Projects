aba63cfea46ecd170697ec9b8a96d8f5fc280cb2
Abhaya <shankarabhaya9@gmail.com>
2023-03-07 18:35:40 +0530

    footer doen

--- IMDB-clone/imdb-clone/src/components/Footer/Footer.jsx
+++ IMDB-clone/imdb-clone/src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Footer.css";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
@@ -7,30 +8,35 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 function Footer() {
   return (
     <div className="footer">
+      <div className="footer__left">Made with ❤️, Connect with me on: </div>
       <div className="socials">
         <a href="https://twitter.com/AbhayaShankar2">
           <TwitterIcon
-            className="icon"
+            // className="icon"
             color="disabled"
-            sx={{ "&:hover": { color: "white" } }}
+            sx={{ color: "#ffffff99 ", "&:hover": { color: "#1d9bf0" } }}
           />
         </a>
         <a href="https://github.com/AbhayaShankar">
-          <GitHubIcon color="disabled" sx={{ "&:hover": { color: "white" } }} />
+          <GitHubIcon
+            color="disabled"
+            sx={{ color: "#ffffff99 ", "&:hover": { color: "#000000" } }}
+          />
         </a>
         <a href="https://www.linkedin.com/in/abhayashankar/">
           <LinkedInIcon
             color="disabled"
-            sx={{ "&:hover": { color: "white" } }}
+            sx={{ color: "#ffffff99 ", "&:hover": { color: "#0a66c2" } }}
           />
         </a>
         <a href="https://www.instagram.com/abhaya_shankar05/">
           <InstagramIcon
             color="disabled"
-            sx={{ "&:hover": { color: "white" } }}
+            sx={{ color: "#ffffff99 ", "&:hover": { color: "#bb2c6f" } }}
           />
         </a>
       </div>
+      <div className="footer__author">Abhaya Shankar</div>
     </div>
   );
 }
